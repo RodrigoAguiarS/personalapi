25f13c67c1b0f4ea4274e1a09d0dad742d2fba56
Rodrigo Aguiar <rodrigo_aguiars@outlook.com>
2021-12-26 19:41:49 -0300

    Initial commit

---
+++ src/main/java/br/com/rodrigo/personalapi/PersonalapiApplication.java
@@ -0,0 +1,13 @@
+package br.com.rodrigo.personalapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonalapiApplication.class, args);
+	}
+
+}
